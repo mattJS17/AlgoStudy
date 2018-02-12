69b14bf94390ae1f2472ee92e1568fe7802ea440
mattJS17 <jhshim96@naver.com>
2018-02-12 16:29:44 +0900

    18/02/12 QUADTREE

--- QUADTREE/QUADTREE/main.cpp
+++ QUADTREE/QUADTREE/main.cpp
@@ -2,14 +2,40 @@
 //  main.cpp
 //  QUADTREE
 //
-//  Created by ShimJaeHoon on 2018. 2. 5..
-//  Copyright © 2018년 ShimJaeHoon. All rights reserved.
+//  Created by ShimJaeHoon on 2017. 7. 27..
+//  Copyright © 2017년 ShimJaeHoon. All rights reserved.
 //
 
 #include <iostream>
+using namespace std;
+
+string func(string::iterator& it) {
+    char h = *it;
+    it++;
+    
+    if (h != 'x')
+        return string(1, h);
+    
+    string first = func(it);
+    string second = func(it);
+    string third = func(it);
+    string forth = func(it);
+    
+    return string("x")+third+forth+first+second;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    string quad;
+    int n;
+    
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cin >> quad;
+        string::iterator it = quad.begin();
+        string a = func(it);
+        cout << a << endl;
+    }
+    
     return 0;
 }
+
